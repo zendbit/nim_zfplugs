4fe3e6790d9ef7202ae35c64b4e3b98b435fe073
Amru Rosyada <amru.rosyada@gmail.com>
2020-07-02 12:56:29 +0700

    update paging

--- zfplugs/paging.nim
+++ zfplugs/paging.nim
@@ -16,7 +16,7 @@ proc genPaging*(data: JsonNode, url: Uri3, perPage: int64, numData: int64): Json
     "numData": numData,
     "lastPage": "",
     "firstPage": ""}
-
+  
   if not data.isNil and data.kind == JsonNodeKind.JArray:
     if currentPage > 0:
       result["page"] = %currentPage
@@ -26,13 +26,12 @@ proc genPaging*(data: JsonNode, url: Uri3, perPage: int64, numData: int64): Json
       result["numData"] = %numData
       result["numPage"] = %numPage
       if currentPage < numPage:
-        let nextPage = currentPage * limit
         url.setQuery("page", $(currentPage + 1))
         result["nextPage"] = % $url
         
-        if currentPage > 1:
-          url.setQuery("page", $(currentPage - 1))
-          result["prevPage"] = % $url
+      if currentPage <= numPage and currentPage > 1:
+        url.setQuery("page", $(currentPage - 1))
+        result["prevPage"] = % $url
 
       url.setQuery("page", $numPage)
       result["lastPage"] = % $url
